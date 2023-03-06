d7d030f8fa5e1732847458e89f3859f0ed3da5ed
MarkBrianK <kariukimarkbrian@gmail.com>
2023-03-06 16:50:55 +0300

    removed image faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ require 'faker'
     name: Faker::Commerce.product_name,
     category: Faker::Commerce.department(max: 1),
     size: Faker::Number.between(from: 1, to: 10),
-    image_url: Faker::LoremFlickr.image(size: "300x300"),
+    image_url: "https://media.istockphoto.com/id/475570206/photo/denim-jacket.jpg?s=612x612&w=0&k=20&c=-RlIxj6nOvJYyIMu_GS8MYUzRLw3IEVwDXen-x5JSKY=",
     price: Faker::Commerce.price(range: 10..1000.0)
   )
   puts "Created cloth #{cloth.id}: #{cloth.name}"
